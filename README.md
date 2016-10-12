4c60c18825204ed0b895fc1e5e8ed06f33fb07e2
Geoffrey Mastenbroek <geoffrey@maschel.com>
2016-10-12 12:13:51 +0200

    Initial project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# RoombaJSSC
+JAVA library to control a roomba using the JSSC serial library.
---
+++ src/main/java/com/maschel/RoombaJSSC.java
@@ -0,0 +1,7 @@
+package com.maschel;
+
+/**
+ * Created by geoffrey on 12/10/2016.
+ */
+public class RoombaJSSC {
+}
