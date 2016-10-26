a14a6d1cb41fd6f57b4102e5c8ec0ef04144b27e
Geoffrey Mastenbroek <geoffrey@maschel.com>
2016-10-26 16:56:56 +0200

    Fixed travis deploy issue

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,4 +7,4 @@ else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -Dcobertura.skip=true -Dcoveralls.skip=true -B -U
